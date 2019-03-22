95bfadb71345a9585728ced48186b63510f74506
aruizm1 <aruizm@ucenfotec.ac.cr>
2019-03-21 20:37:32 -0600

    Eliminacion del error 401 al iniciar la aplicacion

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
 
-import { LoginModalService, AccountService, Account } from 'app/core';
+import { LoginModalService } from 'app/core';
 
 @Component({
     selector: 'jhi-home',
@@ -10,33 +9,11 @@ import { LoginModalService, AccountService, Account } from 'app/core';
     styleUrls: ['home.scss']
 })
 export class HomeComponent implements OnInit {
-    account: Account;
     modalRef: NgbModalRef;
 
-    constructor(
-        private accountService: AccountService,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {}
+    constructor(private loginModalService: LoginModalService) {}
 
-    ngOnInit() {
-        this.accountService.identity().then((account: Account) => {
-            this.account = account;
-        });
-        this.registerAuthenticationSuccess();
-    }
-
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', message => {
-            this.accountService.identity().then(account => {
-                this.account = account;
-            });
-        });
-    }
-
-    isAuthenticated() {
-        return this.accountService.isAuthenticated();
-    }
+    ngOnInit() {}
 
     login() {
         this.modalRef = this.loginModalService.open();
