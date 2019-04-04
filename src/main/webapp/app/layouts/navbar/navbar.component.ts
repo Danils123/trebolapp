c750ec8b6232c6cea3afa788c2959551fe92ff3d
betosr20 <betog19@gmail.com>
2019-04-04 13:48:04 -0600

    Se agregan notificaciones de ofertas al navBar, por ahora trae las de todos los comercios

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -4,6 +4,12 @@ import { AccountService, LoginModalService, LoginService, IUser } from 'app/core
 import { ProfileService } from 'app/layouts/profiles/profile.service';
 import { Router } from '@angular/router';
 import { UserExtraService } from 'app/entities/user-extra';
+import { IOffer } from '../../shared/model/offer.model';
+import { ICommerce } from '../../shared/model/commerce.model';
+import { CommerceService } from 'app/entities/commerce';
+import { OfferService } from 'app/entities/offer';
+import { filter, map } from 'rxjs/operators';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-navbar',
@@ -17,6 +23,8 @@ export class NavbarComponent implements OnInit {
     swaggerEnabled: boolean;
     modalRef: NgbModalRef;
     version: string;
+    offers: IOffer[] = [];
+    informationArray: Information[] = [];
 
     constructor(
         private loginService: LoginService,
@@ -24,7 +32,9 @@ export class NavbarComponent implements OnInit {
         private loginModalService: LoginModalService,
         private router: Router,
         private profileService: ProfileService,
-        public userExtraService: UserExtraService
+        public userExtraService: UserExtraService,
+        public commerceService: CommerceService,
+        public offerService: OfferService
     ) {}
 
     ngOnInit() {
@@ -32,6 +42,8 @@ export class NavbarComponent implements OnInit {
             this.inProduction = profileInfo.inProduction;
             this.swaggerEnabled = profileInfo.swaggerEnabled;
         });
+
+        this.loadInfo();
     }
 
     isAuthenticated() {
@@ -54,4 +66,34 @@ export class NavbarComponent implements OnInit {
     refresh() {
         this.accountService.refreshUser();
     }
+
+    //This method loads the offers from the commerces for the notifications panel
+    loadInfo() {
+        this.commerceService
+            .query()
+            .pipe(
+                filter((res: HttpResponse<ICommerce[]>) => res.ok),
+                map((res: HttpResponse<ICommerce[]>) => res.body)
+            )
+            .subscribe((res: ICommerce[]) => {
+                let informationObject: Information = new Information();
+                res.forEach(commerce => {
+                    if (commerce.offer != null && commerce.offer !== undefined) {
+                        this.offers.push(commerce.offer);
+                        informationObject = new Information();
+                        informationObject.commerceName = commerce.name;
+                        informationObject.offerDescription = commerce.offer.description;
+                        informationObject.commerceId = commerce.id;
+                        informationObject.expirationDate = commerce.offer.expirationDate;
+                        this.informationArray.push(informationObject);
+                    }
+                });
+                console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+                console.log(this.informationArray);
+            });
+    }
+}
+
+class Information {
+    constructor(public commerceId?: number, public commerceName?: string, public offerDescription?: string, public expirationDate?: Date) {}
 }
