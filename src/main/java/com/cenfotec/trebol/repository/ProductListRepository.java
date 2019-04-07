6f4afd337608461fac61d1df9558dbda23227227
aruizm1 <aruizm@ucenfotec.ac.cr>
2019-04-06 23:26:54 -0600

    calendarizar la compra el imput de fecha y hora se agrego

--- src/main/java/com/cenfotec/trebol/repository/ProductListRepository.java
+++ src/main/java/com/cenfotec/trebol/repository/ProductListRepository.java
@@ -4,6 +4,8 @@ import com.cenfotec.trebol.domain.ProductList;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 /**
  * Spring Data  repository for the ProductList entity.
@@ -11,5 +13,5 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface ProductListRepository extends JpaRepository<ProductList, Long> {
-
+    List<ProductList> findByidlistpurchase(Integer idlistpurchase);
 }
--- src/main/webapp/app/entities/product-list/card/card.component.ts
+++ src/main/webapp/app/entities/product-list/card/card.component.ts
@@ -2,20 +2,39 @@ import { Component, Input, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
 import { AccountService } from 'app/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Ilistpurchaseall } from '../../../shared/model/listpurchaseall.model';
 import { IListPurchase } from 'app/shared/model/list-purchase.model';
 import { ListPurchaseService } from 'app/entities/list-purchase';
+import { DatepickerOptions } from 'ng2-datepicker';
+import { IListSchedule } from 'app/shared/model/list-schedule.model';
+import { ListScheduleService } from 'app/entities/list-schedule';
+import { DATE_TIME_FORMAT } from 'app/shared';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import moment = require('moment');
+import { IProductList, ProductList } from 'app/shared/model/product-list.model';
 
 @Component({
     selector: 'jhi-card',
-    templateUrl: './card.component.html',
-    styles: []
+    templateUrl: './card.component.html'
 })
 export class CardComponent implements OnInit {
     @Input() listpurchaseAll: Ilistpurchaseall;
     currentAccount: any;
     eventSubscriber: Subscription;
+    calendarVisible: boolean;
+    listSchedule: IListSchedule;
+    isSaving: boolean;
+    hstep = 1;
+    mstep = 15;
+    ismeridian = true;
+
+    options: DatepickerOptions = {
+        minDate: new Date(Date.now()),
+        placeholder: 'Seleccione una fecha',
+        displayFormat: 'DD/MM/YYYY',
+        useEmptyBarTitle: false
+    };
 
     private swalWithBootstrapButtons = Swal.mixin({
         customClass: {
@@ -29,8 +48,18 @@ export class CardComponent implements OnInit {
         protected jhiAlertService: JhiAlertService,
         protected eventManager: JhiEventManager,
         protected accountService: AccountService,
-        protected listPurchaseService: ListPurchaseService
-    ) {}
+        protected listPurchaseService: ListPurchaseService,
+        protected listScheduleService: ListScheduleService
+    ) {
+        this.calendarVisible = false;
+        this.listSchedule = new class implements IListSchedule {
+            day: string;
+            id: number;
+            productList: IProductList;
+            state: boolean;
+            time: moment.Moment;
+        }();
+    }
 
     ngOnInit() {}
 
@@ -60,5 +89,53 @@ export class CardComponent implements OnInit {
             });
             this.swalWithBootstrapButtons.fire('Eliminada!', 'La lista ha sido eliminada.', 'success');
         });
+    }
+
+    programCalendar() {
+        this.calendarVisible = true;
+    }
+
+    cancelCalendar() {
+        this.calendarVisible = false;
+    }
+
+    saveCalendar() {
+        this.calendarVisible = false;
+        this.isSaving = true;
+        this.listSchedule.state = true;
+        const purchase = this.listpurchaseAll.listpurchase;
+        const productlst = new ProductList(purchase.id);
+        this.listSchedule.productList = productlst;
+        console.log('list compra');
+        console.log(this.listSchedule);
+        /*this.listSchedule.time = this.listSchedule.time != null ? moment(this.listSchedule.time, DATE_TIME_FORMAT) : null;
+        if (this.listSchedule.id !== undefined) {
+            this.subscribeToSaveResponse(this.listScheduleService.update(this.listSchedule));
+        } else {
+            this.subscribeToSaveResponse(this.listScheduleService.create(this.listSchedule));
+        }*/
+    }
+
+    protected subscribeToSaveResponse(result: Observable<HttpResponse<IListSchedule>>) {
+        result.subscribe((res: HttpResponse<IListSchedule>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
+    }
+
+    protected onSaveSuccess() {
+        this.isSaving = false;
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+        });
+
+        Toast.fire({
+            type: 'success',
+            title: 'Fecha agregada satisfactoriamente'
+        });
+    }
+
+    protected onSaveError() {
+        this.isSaving = false;
     }
 }
